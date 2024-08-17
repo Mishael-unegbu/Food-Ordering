f248c6d0954646fec7e439c3cff37688a03a0df2
misheal <misheal.unegbu@halogen-group.com>
2024-08-17 08:06:39 +0100

    navigation added

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,44 +1,54 @@
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet, Image, Pressable } from "react-native";
 
-import EditScreenInfo from '@/src/components/EditScreenInfo';
-import { Text, View,  } from '@/src/components/Themed';
-import Colors from '../constants/Colors';
-//import products from '@/assets/data/products';
+import { Text, View } from "@/src/components/Themed";
+import Colors from "../constants/Colors";
+import { Product } from "../types";
+import { Link } from "expo-router";
 
+export const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/hawaiin.png";
 
-//const product = products[0];
+type ProductListItemProps = {
+  product: Product;
+};
 
-const ProductListItem = ({ product }) => {
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
-    <View style={styles.container}>
-    <Image source={{uri: product.image}}
-    style={styles.image} />
-    <Text style={styles.title}>{product.name}</Text>
-    <Text>${product.price}</Text>
-  </View>
+    <Link href={'/${produce.id}'} asChild>
+      <Pressable style={styles.container}>
+        <Image
+          source={{ uri: product.image || defaultPizzaImage }}
+          style={styles.image}
+          resizeMode="contain"
+        />
+        <Text style={styles.title}>{product.name}</Text>
+        <Text>${product.price}</Text>
+      </Pressable>
+    </Link>
   );
-}
-
+};
 
 export default ProductListItem;
 
 const styles = StyleSheet.create({
-  container: { 
-    backgroundColor: 'white',
+  container: {
+    backgroundColor: "white",
     padding: 10,
     borderRadius: 20,
+    flex: 1,
+    maxWidth: "50%",
   },
   image: {
-    width: '100%',
-    aspectRatio: 1
+    width: "100%",
+    aspectRatio: 1,
   },
   title: {
     fontSize: 18,
-    fontWeight: '600',
+    fontWeight: "600",
     marginVertical: 10,
   },
   price: {
     color: Colors.light.tint,
-    fontWeight: 'bold'
-  }
+    fontWeight: "bold",
+  },
 });
